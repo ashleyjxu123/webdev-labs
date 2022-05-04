722696180ef9606eac56c513215408656b15a934
ashleyjxu123 <ashleyjxu123@gmail.com>
2022-05-04 15:14:25 -0500

    lab6 updates

--- lab06 (1)/01-font-switcher/index.js
+++ lab06 (1)/01-font-switcher/index.js
@@ -2,12 +2,15 @@ const makeBigger = (ev) => {
    // alert('make bigger!');
    console.log("make bigger!")
    const elementClicked = ev.currentTarget;
-   curr_size_h = document.querySelector("h1").style.fontSize;
-   curr_size_d = document.querySelector("div.content").style.fontSize;
+   curr_size_h = parseFloat(window.getComputedStyle(document.querySelector("h1"), null).getPropertyValue('font-size'))
+   curr_size_d  = parseFloat(window.getComputedStyle(document.querySelector("div.content"), null).getPropertyValue('font-size'))
+   // get current font size & increase by certain number of pixels
+   // h1.style.fontSize = (h1Size + 2) + 'px';
+   // parseFloat
    console.log(curr_size_h)
    if (elementClicked.id == "a1"){
-      document.querySelector("h1").style.fontSize = "250%";
-      document.querySelector("div.content").style.fontSize = "200%";
+      document.querySelector("h1").style.fontSize = (curr_size_h + 5) + 'px';
+      document.querySelector("div.content").style.fontSize = (curr_size_d + 5) + 'px';
    }
 };
 
@@ -15,9 +18,13 @@ const makeSmaller = (ev) => {
    // alert('make smaller!');
    console.log("make smaller!")
    const elementClicked = ev.currentTarget;
+
+   curr_size_h = parseFloat(window.getComputedStyle(document.querySelector("h1"), null).getPropertyValue('font-size'))
+   curr_size_d  = parseFloat(window.getComputedStyle(document.querySelector("div.content"), null).getPropertyValue('font-size'))
+
    if (elementClicked.id == "a2"){
-      document.querySelector("h1").style.fontSize = "150%";
-      document.querySelector("div.content").style.fontSize = "100%";
+      document.querySelector("h1").style.fontSize = (curr_size_h - 5) + 'px';
+      document.querySelector("div.content").style.fontSize = (curr_size_d - 5) + 'px';
    }
 };
 
